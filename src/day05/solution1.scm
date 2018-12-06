d7c088e67651726a7f63f41a9f7d3a8694c0e6ee
Kevin Pavao <kpavao84@gmail.com>
2018-12-05 22:51:55 -0600

    refactor day 5 pt 2, use aoc parse lib

--- src/day05/solution1.scm
+++ src/day05/solution1.scm
@@ -2,16 +2,18 @@
 -e solution1 -s
 !#
 (use-modules (aoc parse)
-             (ice-9 rdelim)
+             (ice-9 regex)
              (srfi srfi-1))
 
-(define input (car (with-input-from-file "input.txt" parse-input)))
+(define input (car (with-input-from-file "testinput.txt" parse-input)))
 
 (define (polymers-remaining polymers)
+  "Return the number of polymers remaining after reacting,
+e.g. remove all matching 'Aa/aA', and return the length after all have been removed"
   (let ((re "aA|Aa|bB|Bb|cC|Cc|dD|Dd|eE|Ee|fF|Ff|gG|Gg|hH|Hh|iI|Ii|jJ|Jj|kK|Kk|lL|Ll|mM|Mm|nN|Nn|oO|Oo|pP|Pp|qQ|Qq|rR|Rr|sS|Ss|tT|Tt|uU|Uu|vV|Vv|wW|Ww|xX|Xx|yY|Yy|zZ|Zz"))
-    (do ((polymers polymers (regexp-substitute/global #f re polymers 'pre "" 'post)))
-        ((not (string-match my-regex polymers))
-         (string-length polymers)))))
+    (do ((reacted polymers (regexp-substitute/global #f re reacted 'pre "" 'post)))
+        ((not (string-match re reacted))
+         (string-length reacted)))))
 
 (define (solution1 args)
   (display (polymers-remaining input)))
